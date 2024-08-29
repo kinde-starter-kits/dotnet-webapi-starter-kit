656d6add16a99bee17006deeec1e6ac77ee299da
Leo Chen <coel@users.noreply.github.com>
2024-08-29 10:36:34 +1000

    Add JWT protected routes and basic policies

--- KindeWebAPI/Controllers/WeatherForecastController.cs
+++ KindeWebAPI/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KindeWebAPI.Controllers
@@ -18,13 +19,54 @@ namespace KindeWebAPI.Controllers
       _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+
+    [Authorize]
+    [HttpGet("Protected", Name = "GetWeatherForecastProtected")]
+    public IEnumerable<WeatherForecast> GetProtected()
+    {
+      var user = HttpContext.User;
+      return Enumerable.Range(1, 4).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    [Authorize(Policy = "ReadWeatherPermission")]
+    [HttpGet("PermissionProtected", Name = "GetWeatherForecastPermissionProtected")]
+    public IEnumerable<WeatherForecast> GetPermissionProtected()
+    {
+      return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 0),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    [Authorize(Policy = "AdminRole")]
+    [HttpGet("RoleProtected", Name = "GetWeatherForecastRoleProtected")]
+    public IEnumerable<WeatherForecast> GetRoleProtected()
+    {
+      return Enumerable.Range(1, 3).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(30, 45),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    [HttpGet("Public", Name = "GetWeatherForecastPublic")]
+    public IEnumerable<WeatherForecast> GetPublic()
     {
       return Enumerable.Range(1, 5).Select(index => new WeatherForecast
       {
         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
+        TemperatureC = Random.Shared.Next(16, 32),
         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
       })
       .ToArray();
--- KindeWebAPI/Program.cs
+++ KindeWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+      // These two lines map the Kinde user ID to Identity.Name (optional).
+      options.MapInboundClaims = false;
+      options.TokenValidationParameters.NameClaimType = "sub";
+    });
+
+builder.Services
+    .AddAuthorization(options =>
+    {
+      options.AddPolicy("ReadWeatherPermission",
+        policy => policy.RequireAssertion(
+          context => context.User.Claims.Any(c => c.Type == "permissions" && c.Value == "read:weather")
+        ));
+      options.AddPolicy("AdminRole",
+        policy => policy.RequireAssertion(
+          context => context.User.Claims.Any(c => c.Type == "roles" && c.Value == "admin")
+        ));
+    });
 
 var app = builder.Build();
 
@@ -21,5 +46,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Minimal API examples
+app.MapGet("/Minimal/Public", () => "Hello world!");
+app.MapGet("/Minimal/Protected", (ClaimsPrincipal user) => "Hello " + user?.Identity?.Name)
+  .RequireAuthorization("ReadWeatherPermission");
 
 app.Run();
